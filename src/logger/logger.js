119f065d4ace193f92bf3ccec1190bb5893930b8
Anubhav <anubhav0347@gmail.com>
2022-08-20 21:02:45 +0530

    postApi progress

---
+++ src/logger/logger.js
@@ -0,0 +1,4 @@
+function welcome(){
+console.log("Welcome to my application, my name is Anubhav Kumar and I am part of functionUp plutonium cohort")
+}
+module.exports.Welcome =welcome
---
+++ src/validator/formatter.js
@@ -0,0 +1,14 @@
+function trim(){ 
+console.log(("  Anubhav Kumar").trim())
+}
+
+function lowerCase(){
+console.log(("ANUBHAv").toLowerCase())
+}
+
+function upperCase(){
+console.log(("anubhav").toUpperCase())
+}
+module.exports.trim =trim
+module.exports.lowerCase =lowerCase
+module.exports.upperCase =upperCase
