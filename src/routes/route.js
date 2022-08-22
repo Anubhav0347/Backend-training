0c18e35f9915c1b1cc704ef3e0032a12f0af01d8
Anubhav <anubhav0347@gmail.com>
2022-08-22 22:38:21 +0530

    mongo session 3 and  assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
+const newBookController= require("../controllers/newBookController")
+const AuthorController= require("../controllers/authorController")
+
+
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
@@ -15,5 +20,39 @@ router.get("/getUsersData", UserController.getUsersData)
 router.post("/createBook", BookController.createBook  )
 
 router.get("/getBooksData", BookController.getBooksData)
+
+router.post("/updateBooks", BookController.updateBooks)
+router.post("/deleteBooks", BookController.deleteBooks)
+router.post("/deleteBooks", BookController.deleteBooks)
+
+//MOMENT JS
+const moment = require('moment');
+router.get("/dateManipulations", function (req, res) {
+    
+    // const today = moment();
+    // let x= today.add(10, "days")
+
+    // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
+    // console.log(validOrNot)
+    
+    const dateA = moment('01-01-1900', 'DD-MM-YYYY');
+    const dateB = moment('01-01-2000', 'DD-MM-YYYY');
+
+    let x= dateB.diff(dateA, "days")
+    console.log(x)
+
+    res.send({ msg: "all good"})
+})
+router.post("/ashish",function(req,res){
+    const data=req.query
+    res.send(data)
+})
+
+router.post("/newCreateBook",newBookController.createBook)
+router.post("/createAuthorInfo",AuthorController.createAuthorInfo)
+router.post("/getAuthorId",AuthorController.getAuthorId)
+router.post("/getAuthorName_updatePrice",AuthorController.getAuthorName_updatePrice)
+router.post("/getRangePrice_author",AuthorController.getRangePrice_author)
+
 
 module.exports = router;
