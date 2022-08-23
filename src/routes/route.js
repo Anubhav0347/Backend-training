e52fcb1c1ebcb712a9bb981d94732edfdc9c516b
Anubhav <anubhav0347@gmail.com>
2022-08-24 02:44:53 +0530

    populate and refernce assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const authorController= require("../controllers/authorController")
 const bookController= require("../controllers/bookController")
+const newAuthorController= require("../controllers/newAuthorController")
+const newPublisherController= require("../controllers/newPublisherController")
+const newBookController= require("../controllers/newBookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -17,5 +20,12 @@ router.post("/createBook", bookController.createBook  )
 router.get("/getBooksData", bookController.getBooksData)
 
 router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
+
+router.post("/newcreateAuthor", newAuthorController.createAuthor)
+router.post("/newPublisherController", newPublisherController.createPublisher)
+router.post("/newBookController", newBookController.newCreateBook)
+router.get("/getAllBook", newBookController.getAllBook)
+router.get("/updateValue", newBookController.updateValue)
+router.get("/updatePrice", newBookController.updatePrice)
 
 module.exports = router;
